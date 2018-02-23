a05e175618cbe5b4be1854653dba72677bfb7a4d
mustafaquraish <mustafaq9@gmail.com>
2018-02-23 03:54:29 -0500

    Removed mnist

--- sketch.js
+++ sketch.js
@@ -20,26 +20,6 @@ var nn;
 
 function setup() {
 
-  var dataFileBuffer  = loadStrings('./mnist/train-images-idx3-ubyte');
-  var labelFileBuffer = loadStrings('./mnist/train-labels-idx1-ubyte');
-  var pixelValues     = [];
-
-  // It would be nice with a checker instead of a hard coded 60000 limit here
-  for (var image = 0; image <= 10; image++) {
-    var pixels = [];
-
-    for (var x = 0; x <= 27; x++) {
-      for (var y = 0; y <= 27; y++) {
-        pixels.push(dataFileBuffer[(image * 28 * 28) + (x + (y * 28)) + 15]);
-      }
-    }
-
-    var imageData  = {};
-    imageData[JSON.stringify(labelFileBuffer[image + 8])] = pixels;
-
-    pixelValues.push(imageData);
-  }
-
   nn = new NeuralNetwork(3,[2,3,1]);
 
   for (let i = 0; i < 1000000; i++) {
